82a0801f7d19a682a81b0c850f60ba1e9903febc
Ruben Rodriguez <hola@rubenr.dev>
2023-02-27 12:54:46 +0100

    feat: :sparkles: Some test code added

--- src/components/Bottle/QtySelector.vue
+++ src/components/Bottle/QtySelector.vue
@@ -4,25 +4,7 @@
     v-if="quick"
   >
     <div
-      class="
-        focus:outline-none
-        text-center
-        w-full
-        bg-gray-300
-        font-semibold
-        text-md
-        hover:text-white
-        focus:text-black
-        md:text-basecursor-default
-        flex
-        items-center
-        text-gray-700
-        outline-none
-        justify-center
-        rounded
-        cursor-pointer
-        hover:bg-gray-500
-      "
+      class="focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-white focus:text-black md:text-basecursor-default flex items-center text-gray-700 outline-none justify-center rounded cursor-pointer hover:bg-gray-500"
       v-if="!showAction"
       @click="showAction = true"
     >
@@ -30,25 +12,7 @@
     </div>
     <div
       v-else
-      class="
-        focus:outline-none
-        text-center
-        w-full
-        bg-pink-900
-        font-semibold
-        text-md
-        hover:text-white
-        text-white
-        focus:text-black
-        md:text-basecursor-default
-        flex
-        items-center
-        outline-none
-        justify-center
-        rounded
-        cursor-pointer
-        hover:bg-pink-800
-      "
+      class="focus:outline-none text-center w-full bg-pink-900 font-semibold text-md hover:text-white text-white focus:text-black md:text-basecursor-default flex items-center outline-none justify-center rounded cursor-pointer hover:bg-pink-800"
     >
       <button class="" @click="decreaseQty()">
         <span class="">{{ $t('cellar.drink') }}</span>
@@ -60,17 +24,7 @@
     v-else
   >
     <div
-      class="
-        bg-gray-300
-        text-gray-600
-        hover:text-gray-700 hover:bg-gray-400
-        h-full
-        w-20
-        rounded-l
-        cursor-pointer
-        outline-none
-        flex
-      "
+      class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none flex"
       @click="decreaseQty()"
     >
       <font-awesome-icon
@@ -79,35 +33,12 @@
       ></font-awesome-icon>
     </div>
     <div
-      class="
-        focus:outline-none
-        text-center
-        w-full
-        bg-gray-300
-        font-semibold
-        text-md
-        focus:text-black
-        md:text-basecursor-default
-        flex
-        items-center
-        text-gray-700
-        outline-none
-        justify-center
-      "
+      class="focus:outline-none text-center w-full bg-gray-300 font-semibold text-md focus:text-black md:text-basecursor-default flex items-center text-gray-700 outline-none justify-center"
     >
       {{ qty }}
     </div>
     <div
-      class="
-        bg-gray-300
-        text-gray-600
-        hover:text-gray-700 hover:bg-gray-400
-        h-full
-        w-20
-        rounded-r
-        cursor-pointer
-        flex
-      "
+      class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer flex"
       @click="increaseQty()"
     >
       <font-awesome-icon
@@ -139,10 +70,12 @@
     },
     methods: {
       increaseQty() {
+        console.log('increaseQty')
         this.$emit('incrementQty', this.qty + 1)
       },
       decreaseQty() {
         if (this.qty != 0) {
+          console.log('decreaseQty')
           this.$emit('decrementQty', this.qty - 1)
         }
         this.showAction = false
