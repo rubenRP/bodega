da153893196c3de8fd8692eba9fc1cdc4ffcd7b8
Ruben Rodriguez <hola@rubenr.dev>
2023-04-11 12:10:53 +0200

    feat: :construction: Vuetify added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
-  <Header />
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet>
+              <v-list>
+                <v-list-item> HOLA </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+          <v-col></v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+
   <!--<Notifications />-->
   <main class="container">
     <router-view />
