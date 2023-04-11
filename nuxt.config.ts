4ffca50b66e3de9e7dba57fa7cfee4e442e813b5
Ruben Rodriguez <ruben.rodriguez@scalefast.com>
2023-04-11 15:07:47 +0200

    Revert "More vuetify added"
    This reverts commit 1e927ea1078af43bbb9dd293668da4a81369d705.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,9 @@ import messages from "./i18n/es";
 export default defineNuxtConfig({
   modules: ["nuxt-vitest", "@nuxtjs/i18n", "@pinia/nuxt"],
   css: [
-    "vuetify/lib/styles/main.sass",
     "@fortawesome/fontawesome-svg-core/styles.css",
     "~/assets/styles/main.scss",
   ],
-  build: {
-    transpile: ["vuetify"],
-  },
-  vite: {
-    define: {
-      "process.env.DEBUG": false,
-    },
-  },
   i18n: {
     vueI18n: {
       locale: "es",
